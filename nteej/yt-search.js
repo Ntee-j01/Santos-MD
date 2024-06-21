a61feb92c12addd46104b4746685b64f5e2754d9
Ntee-j01 <140890199+Ntee-j01@users.noreply.github.com>
2024-06-21 23:32:26 +0200

    Update yt-search.js

--- nteej/yt-search.js
+++ nteej/yt-search.js
@@ -1,4 +1,4 @@
-const { zokou } = require("../framework/zokou");
+const { zokou } = require("../framework/nteej");
 const { getytlink, ytdwn } = require("../framework/ytdl-core");
 const yts = require("yt-search");
 const ytdl = require('ytdl-core');
@@ -21,7 +21,7 @@ zokou({ nomCom: "yts", categorie: "Search", reaction: "✋" }, async (dest, zk, 
     for (let i = 0; i < 10; i++) {
       captions += `----------------\nTitle: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
     }
-    captions += "\n======\n*powered by Zokou-Md*";
+    captions += "\n======\n*powered by Nteej-Md*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
